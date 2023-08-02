4e215f61a158aff6b24e364238a49db0d960d118
Wes Storms <wes.storms@truexpro.com>
2023-08-02 07:56:32 +0530

    New Changes

---
+++ .history/src/components/Header_20230802072539.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled form "styled-components"
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+
+}
+
+const Container
---
+++ .history/src/components/Header_20230802072832.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components"
+import logo from "../assets/logo.png"
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+
+}
+
+const Container = styled.div``;
